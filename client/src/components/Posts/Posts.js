9e40773886757ac4072fa49517982d549f0c09d2
Brendan Cariota <brendan@cariota.com>
2021-07-27 09:47:58 -0500

    setup backend models and implemented basic frontend ui

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Post from './Post/Post'
+
+import useStyles from './styles'
+
+function Posts() {
+
+    const classes = useStyles()
+
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post />
+            <Post />
+        </>
+    )
+}
+
+export default Posts
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,32 @@
 // Create handlers for all post routes
 
-export const getPosts = (req, res) => {
-    res.send('This WORKS!')
+import PostMessage from '../models/postMessage.js'
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find()
+
+        console.log(postMessages)
+
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+export const createPost = async (req, res) => {
+    // Get the information sent in the req
+    const post = req.body
+
+    // Syntax to create a new post with our Post Schema
+    const newPost = new PostMessage(post)
+
+    try {
+        // Saves new post in database
+        await newPost.save()
+
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
 }
