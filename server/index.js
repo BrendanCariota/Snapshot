97247159daaab276be77b4c72896ab67f807f55c
Brendan Cariota <brendan@cariota.com>
2021-07-29 02:43:53 -0500

    updated server and deployed to backend, site is live

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 import cors from 'cors'
 
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config()
 
 app.use(express.json({ limit: "30mb", extended: true }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
@@ -14,13 +15,15 @@ app.use(cors())
 // Sets up where every route inside the postRoutes is going to start with posts - MUST BE BELOW CORS INITIALIZATION
 app.use('/posts', postRoutes)
 
-// Connect to our MongoDB Cluster
-const CONNECTION_URL = `mongodb+srv://BrendanCariota:Cariota1.@cluster0.i14ly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+app.get('/', (req, res) => {
+    res.send('Hello to Snapshot API')
+})
+
 // PORT we are running the server on
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 // Connecting to the Database with our URL and passing in the object to prevent warnings
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((err) => console.log(err.message))
 
